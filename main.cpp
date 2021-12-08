2a9810d8a3fcc150304fbf269a1b3003128aacba
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-12-08 10:20:52 +0530

    Fix MSVC build
    * Use XMVector accessor methods as subscript operator
      doesn't work with MSVC compiler
    * CMake 3.18 on Windows doesn't support generator expression of
      CONFIG with multiple configuration names
    * MSYS2's MinGW + CMake didn't require any changes

--- main.cpp
+++ main.cpp
@@ -19,11 +19,18 @@
 // all DirectX Math symbols start with ‘XM’ prefix
 using namespace DirectX;
 
+std::ostream& operator<<(std::ostream &os, XMVECTOR const &v) {
+  return os << XMVectorGetX(v) << '\t'
+            << XMVectorGetY(v) << '\t'
+            << XMVectorGetZ(v) << '\t'
+            << XMVectorGetW(v);
+}
+
 int main() {
   XMVECTOR v = {1.0f, 1.0f};
   XMVECTOR vQuat = XMQuaternionRotationAxis(XMVECTOR{0, 0, 1, 0}, XM_PIDIV2);
   auto mRot = XMMatrixRotationQuaternion(vQuat);
-  std::cout << v[0] << '\t' << v[1] << '\t' << v[2] << '\t' << v[3] << '\n';
+  std::cout << v << '\n';
   v = XMVector4Transform(v, mRot);
-  std::cout << v[0] << '\t' << v[1] << '\t' << v[2] << '\t' << v[3] << '\n';
+  std::cout << v << '\n';
 }
